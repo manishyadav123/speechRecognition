389f7a9bdbd6fd905adaa34629a91d5ce63542ba
Manish yadav <44017131+manishyadav123@users.noreply.github.com>
2020-05-06 17:50:49 +0530

    Add files via upload

---
+++ speech.py
@@ -0,0 +1,10 @@
+import speech_recognition as speechr
+recog=speechr.Recognizer()
+with speechr.Microphone() as source:
+    print("Speak Anything :")
+    audio=recog.listen(source)
+    try:
+        text=recog.recognize_google(audio)
+        print("Text Form : {}".format(text))
+    except:
+        print("Sorry unable to recognize your voice")
